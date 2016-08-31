649595616f803b7466fbfb68ade0351bf3b30fb4
constantine <const.bon@gmail.com>
2016-08-31 22:56:01 +0300

    Added pyOpenSSL module.

--- test/genca.sh
+++ test/genca.sh
@@ -8,11 +8,11 @@ openssl req -x509 -new -key rootCA.key -days 36500 -out rootCA.crt
 
 #custom key
 function custom {
-openssl genrsa -out server101.mycloud.key 2048
-openssl req -new -key server101.mycloud.key -out server101.mycloud.csr
-openssl x509 -req -in server101.mycloud.csr \
+openssl genrsa -out server102.mycloud.key 2048
+openssl req -new -key server102.mycloud.key -out server102.mycloud.csr
+openssl x509 -req -in server102.mycloud.csr \
                 -CA rootCA.crt -CAkey rootCA.key \
-                -CAcreateserial -out server101.mycloud.crt -days 5000
+                -CAcreateserial -out server102.mycloud.crt -days 5000
 }
 
 custom
