f924d627f334f2af4ac9f7efd881e905e0d5f02a
constantine <const.bon@gmail.com>
2016-12-06 21:02:48 +0200

    Added download option for certificates and CRL

--- certs/urls.py
+++ certs/urls.py
@@ -9,7 +9,9 @@ urlpatterns = [
     url(r'^create_ca/$', views.CreateCACertificateView.as_view(), name='ca_create'),
     url(r'^create/$', views.CreateCertificateView.as_view(), name='create'),
     url(r'^show_ca/ca_(?P<pk>[0-9]+)/$', views.CADetailView.as_view(), name='ca_detail'),
-    url(r'^show_ca/ca_(?P<pk>[0-9]+)/get_crl$', views.GetCRLView.as_view(), name='get_crl'),
+    url(r'^show_ca/ca_(?P<pk>[0-9]+)/get_crl$', views.get_crl, name='get_crl'),
+    url(r'^show_ca/ca_(?P<pk>[0-9]+)/get_certificates$',
+        views.get_certificates, name='get_certificates'),
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^(?P<pk>[0-9]+)/revoke$', views.revoke, name='revoke'),
 ]
--- certs/views.py
+++ certs/views.py
@@ -1,18 +1,20 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse
+import zipfile
 
 from django.shortcuts import get_object_or_404, render, redirect
-from django.http import HttpResponseRedirect
+from django.http import StreamingHttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.core.urlresolvers import reverse_lazy
 from django.views import generic
 from django.views.generic.edit import CreateView, FormView
+from wsgiref.util import FileWrapper
 
 from .models import Certificate, CACertificate
-
 from .forms import CertificateForm, CACertificateForm
 
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
 
 class IndexView(generic.ListView):
     template_name = 'certs/index.html'
@@ -79,17 +81,40 @@ class CreateCACertificateView(FormView):
         return super(CreateCACertificateView, self).form_valid(form)
 
 
-class GetCRLView(generic.DetailView):
-    model = CACertificate
-    template_name = 'certs/crl.html'
+def get_crl(request, pk):
+    ca_cert = get_object_or_404(CACertificate, pk=pk)
+    crl = Certificate.get_crl(ca_cert)
+    temp = StringIO()
+    with zipfile.ZipFile(temp, 'w') as archive:
+            archive.writestr('crl.pem', str(crl))
 
-    def get_context_data(self, **kwargs):
-        # Call the base implementation first to get a context
-        context = super(GetCRLView, self).get_context_data(**kwargs)
-        # Search revoked certificates with CA certificate 'ca_cert' in table Certificate
-        crl = Certificate.get_crl(kwargs['object'])
-        context['crl'] = crl
-        return context
+    response = StreamingHttpResponse(FileWrapper(temp), content_type='application/zip')
+    response['Content-Disposition'] = 'attachment; filename="crl.zip"'
+    response['Content-Length'] = temp.tell()
+
+    temp.seek(0)
+
+    return response
+
+
+def get_certificates(request, pk):
+    cert_list = Certificate.objects.filter(ca_cert=pk, revoked=False)
+    temp = StringIO()
+    with zipfile.ZipFile(temp, 'w') as archive:
+        for cert in cert_list:
+            name = cert.commonName
+            if name == '':
+                name = cert.id
+            archive.writestr('{}.crt'.format(name), str(cert.cert))
+            archive.writestr('{}.key'.format(name), str(cert.key))
+
+    response = StreamingHttpResponse(FileWrapper(temp), content_type='application/zip')
+    response['Content-Disposition'] = 'attachment; filename="certificates.zip"'
+    response['Content-Length'] = temp.tell()
+
+    temp.seek(0)
+
+    return response
 
 
 def revoke(request, pk):
